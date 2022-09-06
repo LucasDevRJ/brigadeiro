092dca3e920bee7738757c41a8eba71a63f0efb8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-05 21:40:38 -0300

    Recuperando conexão com o banco

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaConexao.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaConexao.java
@@ -1,10 +1,14 @@
 package com.github.lucasdevrj.brigadeiro.teste;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestaConexao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws SQLException {
+		//Recuperar conexão
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "root", "root");
 	}
 
 }
