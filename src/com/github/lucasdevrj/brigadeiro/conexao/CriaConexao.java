cdc059ae3e5dd6b76b91e342e3adea7ac461ff71
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-06 18:38:12 -0300

    Recuperando conexão com o banco

--- src/com/github/lucasdevrj/brigadeiro/conexao/CriaConexao.java
+++ src/com/github/lucasdevrj/brigadeiro/conexao/CriaConexao.java
@@ -1,5 +1,13 @@
 package com.github.lucasdevrj.brigadeiro.conexao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class CriaConexao {
 
+	public Connection conecta() throws SQLException {
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/brigadeiro?useTimezone=true&serverTimezone=UTC", "root", "root");
+		return conexao;
+	}
 }
