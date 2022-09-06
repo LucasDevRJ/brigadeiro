aa8323bbd071f90bdc4b1b3933e28d537ce76d96
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-05 22:26:37 -0300

    Criação dos Statements, execução e atributos

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaListagem.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaListagem.java
@@ -2,7 +2,10 @@ package com.github.lucasdevrj.brigadeiro.teste;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
 public class TestaListagem {
 
@@ -10,7 +13,22 @@ public class TestaListagem {
 		//Recuperar conexão com o banco
 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/brigadeiro?useTimezone=true&serverTimezone=UTC", "root", "root");
 		
+		//Utilizar comandos do banco de dados (statements)
+		Statement comandosSql = conexao.createStatement();
+		//Execução dos comandos SQL
+		comandosSql.execute("SELECT * FROM DOCE");
 		
+		//Pegar resultado da lista de doces
+		ResultSet conteudo = comandosSql.getResultSet();
+		
+		//Laço que verifica se tem próximo doce na lista
+		while (conteudo.next()) {
+			//Pegar atributos da tabela
+			Integer id = conteudo.getInt(1);
+			String nome = conteudo.getString(2);
+			String descricao = conteudo.getString(3);
+			Float preco = conteudo.getFloat(4);
+		}
 		
 		conexao.close();
 	}
