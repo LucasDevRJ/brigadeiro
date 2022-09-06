0d37a1d73e68bfb5ce1c1616c8a1a5dfd690a6d3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-05 22:09:19 -0300

    Recuperação de conexão com o banco

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaListagem.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaListagem.java
@@ -1,9 +1,17 @@
 package com.github.lucasdevrj.brigadeiro.teste;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestaListagem {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		//Recuperar conexão com o banco
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/brigadeiro?useTimezone=true&serverTimezone=UTC", "root", "root");
 		
+		
+		
+		conexao.close();
 	}
-
 }
