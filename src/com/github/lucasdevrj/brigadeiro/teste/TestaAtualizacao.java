121314ba6a7d2086509add39c95d9f325a02ebfc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-07 10:42:55 -0300

    Classe para atualizar dados finalizada

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaAtualizacao.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaAtualizacao.java
@@ -2,14 +2,26 @@ package com.github.lucasdevrj.brigadeiro.teste;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.github.lucasdevrj.brigadeiro.conexao.CriaConexao;
 
 public class TestaAtualizacao {
 
 	public static void main(String[] args) throws SQLException {
+		//Criação de conexão com o banco
 		CriaConexao criaConexao = new CriaConexao();
 		Connection conexao = criaConexao.conecta();
+		
+		//Criação dos comandos SQL
+		Statement comandos = conexao.createStatement();
+		comandos.execute("UPDATE DOCE SET GRAMAS = 30.00 WHERE DOCE_ID = 1");
+		comandos.execute("UPDATE DOCE SET GRAMAS = 27.00 WHERE DOCE_ID = 2");
+		comandos.execute("UPDATE DOCE SET GRAMAS = 280.00 WHERE DOCE_ID = 3");
+		
+		Integer linhasAtualizadas = comandos.getUpdateCount();
+		System.out.println("Número de linhas atualizadas: " + linhasAtualizadas);
+		
 	}
 
 }
