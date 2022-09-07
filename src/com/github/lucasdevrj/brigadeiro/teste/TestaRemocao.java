6c020967c11254550080ea03f86e55363bb122e0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-07 09:57:34 -0300

    Criando conexão com o banco na classe para testar remoção

---
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaRemocao.java
@@ -0,0 +1,14 @@
+package com.github.lucasdevrj.brigadeiro.teste;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.github.lucasdevrj.brigadeiro.conexao.CriaConexao;
+
+public class TestaRemocao {
+	
+	public static void main(String[] args) throws SQLException {
+		CriaConexao criaConexao = new CriaConexao();
+		Connection conexao = criaConexao.conecta();
+	}
+}
