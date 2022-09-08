9ed0d79ab2d4cfd354e02257a96b05c5574f22c5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-08 17:58:34 -0300

    Tratamento de erro com o rollback

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaInsercaoComParametro.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaInsercaoComParametro.java
@@ -16,20 +16,27 @@ public class TestaInsercaoComParametro {
 		Connection conexao = criaConexao.conecta();
 		conexao.setAutoCommit(false); //assumir o controle das transações do JDBC
 		
-		//Para usar comandos SQL
-		//prepareStatement para gerenciar os atributos do banco proibindo comandos SQL dentro deles, assim forncendo mais segurança
-		PreparedStatement comandos = conexao.prepareStatement("INSERT INTO DOCE (NOME, DESCRICAO, PRECO, GRAMAS, UNIDADES) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS); //retorna o ID gerado																							
-		
-		//Chamando método e com parâmetros criados
-		adicionaVariavel("Pé de moleque", "Pé de moleque com leite condensado", 5.00f, 84.00, 67, comandos);
-		adicionaVariavel("Danoninho", "Danoninho caseiro", 7.00f, 300.00, 43, comandos);
-		
-		//irá dar um commit caso não haja erro nenhum
-		conexao.commit();
-		
-		//Fechando métodos
-		comandos.close();
-		conexao.close();
+		try {
+			//Para usar comandos SQL
+			//prepareStatement para gerenciar os atributos do banco proibindo comandos SQL dentro deles, assim forncendo mais segurança
+			PreparedStatement comandos = conexao.prepareStatement("INSERT INTO DOCE (NOME, DESCRICAO, PRECO, GRAMAS, UNIDADES) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS); //retorna o ID gerado																							
+			
+			//Chamando método e com parâmetros criados
+			adicionaVariavel("Pé de moleque", "Pé de moleque com leite condensado", 5.00f, 84.00, 67, comandos);
+			adicionaVariavel("Danoninho", "Danoninho caseiro", 7.00f, 300.00, 43, comandos);
+			
+			//irá dar um commit caso não haja erro nenhum
+			conexao.commit();
+			
+			//Fechando métodos
+			comandos.close();
+			conexao.close();
+			
+		} catch (Exception erro) {
+			erro.printStackTrace();
+			System.out.println("Roolback executado!!");
+			conexao.rollback(); //para impedir que seja executada a transição com erro
+		}
 	}
 
 	private static void adicionaVariavel(String nome, String descricao, Float preco, Double gramas, Integer unidades,
@@ -41,9 +48,9 @@ public class TestaInsercaoComParametro {
 		comandos.setDouble(4, gramas);
 		comandos.setInt(5, unidades);
 		
-//		if (nome.equals("Danoninho")) {
-//			throw new RuntimeException("Não foi possível o doce!!!");
-//		}
+		if (nome.equals("Danoninho")) {
+			throw new RuntimeException("Não foi possível o doce!!!");
+		}
 		
 		comandos.execute(); //para executar os comandos SQL
 				
