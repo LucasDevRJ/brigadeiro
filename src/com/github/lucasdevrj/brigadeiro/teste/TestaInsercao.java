a34faaed5b9159f7a544329cf44ef088209d496f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-09 19:45:05 -0300

    Refatoração da classe inserção

--- src/com/github/lucasdevrj/brigadeiro/teste/TestaInsercao.java
+++ src/com/github/lucasdevrj/brigadeiro/teste/TestaInsercao.java
@@ -12,20 +12,21 @@ public class TestaInsercao {
 	public static void main(String[] args) throws SQLException {
 		//Criando conexão com o banco
 		CriaConexao criaConexao = new CriaConexao();
-		Connection conexao = criaConexao.conecta();
+		try (Connection conexao = criaConexao.conecta()) {
 		
-		//Para usar comandos SQL
-		Statement comandos = conexao.createStatement(); 																								//retorna o ID gerado
-		comandos.execute("INSERT INTO DOCE (NOME, DESCRICAO, PRECO, GRAMAS, UNIDADES) VALUES ('Pudim', 'Pudim de leite condensado', 6.00, 0.280, 34)", Statement.RETURN_GENERATED_KEYS);
-		
-		//Pegando o conteúdo do banco pegando ID do banco
-		ResultSet conteudo = comandos.getGeneratedKeys();
-		
-		//Laço que verifica se tem um próximo item no banco e executa funções
-		while (conteudo.next()) {
-			Integer id = conteudo.getInt(1);
-			System.out.println("O ID criado é " + id);
+			//Para usar comandos SQL
+			Statement comandos = conexao.createStatement(); 																								//retorna o ID gerado
+			comandos.execute("INSERT INTO DOCE (NOME, DESCRICAO, PRECO, GRAMAS, UNIDADES) VALUES ('Pudim', 'Pudim de leite condensado', 6.00, 0.280, 34)", Statement.RETURN_GENERATED_KEYS);
+			
+			//Pegando o conteúdo do banco pegando ID do banco
+			try (ResultSet conteudo = comandos.getGeneratedKeys()) {
+			
+				//Laço que verifica se tem um próximo item no banco e executa funções
+				while (conteudo.next()) {
+					Integer id = conteudo.getInt(1);
+					System.out.println("O ID criado é " + id);
+				}
+			}
 		}
 	}
-
 }
