076b779d52c00900cfad146b5243202863213202
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-10 18:32:52 -0300

    Adição do data source

--- src/com/github/lucasdevrj/brigadeiro/conexao/CriaConexao.java
+++ src/com/github/lucasdevrj/brigadeiro/conexao/CriaConexao.java
@@ -4,7 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class CriaConexao {
+	
+	//Implementar a interface data source, para criarmos a conexão com o banco
+	public CriaConexao() {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setJdbcUrl("jdbc:mysql://localhost/brigadeiro?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUser("root");
+		dataSource.setPassword("root");
+	}
 
 	//Criando conexão com o banco de dados
 	public Connection conecta() throws SQLException {
