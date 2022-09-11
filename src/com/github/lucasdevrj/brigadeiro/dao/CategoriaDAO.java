4e327967d14f935a4077b0a6f408fcbfc5c91104
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-11 18:27:38 -0300

    Adição da classe Categoria

--- src/com/github/lucasdevrj/brigadeiro/dao/CategoriaDAO.java
+++ src/com/github/lucasdevrj/brigadeiro/dao/CategoriaDAO.java
@@ -1,6 +1,13 @@
 package com.github.lucasdevrj.brigadeiro.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.lucasdevrj.brigadeiro.modelo.Categoria;
 
 public class CategoriaDAO {
 
@@ -8,5 +15,21 @@ public class CategoriaDAO {
 
 	public CategoriaDAO(Connection conexao) {
 		this.conexao = conexao;
+	}
+	
+	public List<Categoria> listar() throws SQLException {
+		List<Categoria> categorias = new ArrayList<>();
+		
+		String sql = "SELECT * FROM CATEGORIA";
+		
+		try (PreparedStatement comandosSQL = conexao.prepareStatement(sql)) {
+			comandosSQL.execute();
+			
+			try (ResultSet conteudo = comandosSQL.getResultSet()) {
+				while (conteudo.next()) {
+					Categoria categoria = new Categoria(conteudo.getInt(1), conteudo.getString(2));
+				}
+			}
+		}
 	}
 }
---
+++ src/com/github/lucasdevrj/brigadeiro/modelo/Categoria.java
@@ -0,0 +1,12 @@
+package com.github.lucasdevrj.brigadeiro.modelo;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+}
