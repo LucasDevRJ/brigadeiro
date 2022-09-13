440e06a3871911ef97d7f14f9b1573b6e4d02942
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-13 18:55:38 -0300

    Criação da conexão com o banco na categoria controladora

--- src/com/github/lucasdevrj/brigadeiro/controller/CategoriaController.java
+++ src/com/github/lucasdevrj/brigadeiro/controller/CategoriaController.java
@@ -1,11 +1,22 @@
 package com.github.lucasdevrj.brigadeiro.controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.lucasdevrj.brigadeiro.conexao.CriaConexao;
+import com.github.lucasdevrj.brigadeiro.dao.CategoriaDAO;
 import com.github.lucasdevrj.brigadeiro.modelo.Categoria;
 
 public class CategoriaController {
+	
+	private CategoriaDAO categoriaDAO;
+	
+	public CategoriaController() throws SQLException {
+		Connection conexao = new CriaConexao().conecta();
+		this.categoriaDAO = new CategoriaDAO(conexao);
+	}
 
 	public List<Categoria> listar() {
 		List<Categoria> categorias = new ArrayList<Categoria>();
